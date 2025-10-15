3b18477d6618ef249d3354295278b55a3031bf79
thk7964 <yjs7894@gmail.com>
2025-10-15 14:43:31 +0900

    사칙연산 기능 추가

--- src/Main.java
+++ src/Main.java
@@ -11,12 +11,38 @@ public class Main {
             String a = sc.nextLine();
 
             //종료할지 안할지 확인
-            if (a.equals("exit")) {
+            if (a.equals("exit")) {//입력 받은 값이 exit인지 확인
                 System.out.println("=========== 계산기 종료 ===========");
                 break;
 
-            } else{
-                System.out.println("입력 받은 값: "+a);
+            } else if (a.matches("-?\\d+")) { //숫자인지를 판별함
+                System.out.println("정수를 입력하세요:");
+                String b = sc.nextLine();
+
+                if (b.matches("-?\\d+")) {
+                    int num1 = Integer.parseInt(a);
+                    int num2 = Integer.parseInt(b);
+
+                    System.out.println("사칙연산을 입력하세요 : ");
+                    String c = sc.nextLine();
+
+                    if (c.equals("+")) {
+                        System.out.println("결과 : " + (num1 + num2));
+                    } else if (c.equals("-")) {
+                        System.out.println("결과 : " + (num1 + num2));
+                    } else if (c.equals("*")) {
+                        System.out.println("결과 : " + (num1 * num2));
+                    } else if (c.equals("/")) {
+                        System.out.println("결과 : " + (num1 / num2));
+                    } else {
+                        System.out.println("사칙연산이 아닙니다.");
+                    }
+
+                } else {
+                    System.out.println("정수가 아닙니다.");
+                }
+            } else {
+                System.out.println("정수나 exit만 입력 가능합니다.\n다시 입력하세요");
             }
         }
     }
