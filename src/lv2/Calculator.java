56b89a2b26de4f5c9a9f68668271ec7a2ec4683d
thk7964 <yjs7894@gmail.com>
2025-10-15 17:38:21 +0900

    ArrayList 데이터 삭제하는 기능 추가

--- src/lv2/Calculator.java
+++ src/lv2/Calculator.java
@@ -8,22 +8,22 @@ public class Calculator {
         this.result = 0;
     }
 
-    public int sum(int a, int b) {
+    public int sum(int a, int b) {//더하기
         this.result = a + b;
         return this.result;
     }
-    public int sub(int a, int b) {
+    public int sub(int a, int b) {//빼기
         this.result = a - b;
         return this.result;
     }
-    public int mul(int a, int b) {
+    public int mul(int a, int b) {//곱하기
         this.result = a * b;
         return this.result;
     }
-    public int div(int a, int b) {
+    public int div(int a, int b) {//나누기
 
         if(b == 0){
-            throw new ArithmeticException("0으로 나눌 수 없습니다.");
+            throw new ArithmeticException("0으로 나눌 수 없습니다.");//예외 의도 적으로 발생 시킴
         }
         this.result = a / b;
         return this.result;
--- src/lv2/Main.java
+++ src/lv2/Main.java
@@ -14,19 +14,22 @@ public class Main {
         Calculator calc = new Calculator();
 
         while (true) {
+
             boolean flag = false;
+
             System.out.println("정수를 입력하세요(exit 입력 시 종료) : ");
             String input1 = sc.nextLine();
+
             //종료할지 안할지 확인
             if (input1.equals("exit")) {//입력 받은 값이 exit인지 확인
                 System.out.println("=========== 계산기 종료 ===========");
                 break;
 
-            } else if (input1.matches("-?\\d+")) { //숫자인지를 판별함
+            } else if (input1.matches("-?\\d+")) { //숫자인지 확인
                 System.out.println("정수를 입력하세요:");
                 String input2 = sc.nextLine();
 
-                if (input2.matches("-?\\d+")) {
+                if (input2.matches("-?\\d+")) { //숫자인지 확인
                     int num1 = Integer.parseInt(input1);
                     int num2 = Integer.parseInt(input2);
                     int result = 0 ;
@@ -34,6 +37,7 @@ public class Main {
 
                     System.out.println("사칙연산을 입력하세요 (+, -, *, /) : ");
                     String c = sc.nextLine();
+
                     try{
                         switch (c) {
                             case "+" :
@@ -60,7 +64,7 @@ public class Main {
                         System.out.println(e.getMessage());
                         flag2 = false;
                     }
-                    if (flag2){
+                    if (flag2){//ArrayList 데이터 추가
                         System.out.println("결과 : "+result);
                         list.add(result);
                         flag = true;
@@ -72,16 +76,36 @@ public class Main {
             } else {
                 System.out.println("정수나 exit만 입력 가능합니다.\n다시 입력하세요");
             }
+
             while (flag) {
+
                 System.out.println("기록을 확인 하시겠습니까? (Y/N)");
                 String input3 = sc.nextLine();
                 if (input3.equals("Y") || input3.equals("y")) {
+
                     System.out.println("=====지금까지 계산한 합계 조회=====");
                     System.out.println(list);
 
-
+                   break;
                 }
                 else if (input3.equals("N")|| input3.equals("n")) {
+                    break;
+                }
+                else {
+                    System.out.println("형식에 맞게 입력해주세요");
+                }
+            }
+            while (flag) { //ArrayList 데이터 삭제
+                System.out.println("처음으로 기록된 데이터를 삭제 하시겠습니까? (Y/N)");
+                String input4 = sc.nextLine();
+                if (input4.equals("Y") || input4.equals("y")) {
+                    list.remove(0);// 처음 기록된 데이터를 삭제한다.
+                    System.out.println("=====지금까지 계산한 합계 조회=====");
+                    System.out.println(list);
+                    break;
+
+                }
+                else if (input4.equals("N")|| input4.equals("n")) {
                     break;
                 }
                 else {
