d71dc5bac10c57c78f0f033ca310875499add00e
thk7964 <yjs7894@gmail.com>
2025-10-17 16:05:04 +0900

    calculator에 제네릭을 적용해서 사칙연산을 하는 함수를 만들었습니다.

--- src/lv3/App.java
+++ src/lv3/App.java
@@ -1,128 +1,97 @@
 package lv3;
 
 
+import java.util.*;
 
-import java.util.ArrayList;
-import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
 
+        //ArrayList 활용
+        ArrayList<Double> list = new ArrayList<>();
+
+        System.out.println("=========== 계산기 시작 ===========");
         Scanner sc = new Scanner(System.in);
-        System.out.println("사칙연산을 입력하세요 (+, -, *, /) : ");
-        String c = sc.nextLine();
-        OperatorType op =OperatorType.fromSymbol(c);
-        System.out.println(op.getSymbol());
+        ArithmeticCalculator calc = new ArithmeticCalculator();
+        while (true) {
+
+            boolean flag = false;
+            double result = 0;
+            boolean flag2 = true;
+
+            System.out.println("계산기 시작 = 1 , 계산기 기록 = 2 , 처음 기록된 데이터 삭제 = 3 , 종료 = exit 를 입력하시오 :  ");
+            String ck = sc.nextLine();
+            switch (ck) {
+                case "1":
+                    System.out.println("정수를 입력하세요: ");
+                    String input1 = sc.nextLine();
 
 
+                    System.out.println("정수를 입력하세요:");
+                    String input2 = sc.nextLine();
+
+                    System.out.println("사칙연산을 입력하세요 (+, -, *, /) : ");
+                    String input3 = sc.nextLine();
 
 
+                    double num1 = Double.parseDouble(input1);
+                    double num2 = Double.parseDouble(input2);
+                    try {
+                        OperatorType op = OperatorType.fromSymbol(input3);
+                        System.out.println(op.getSymbol());
+                        calc.calculator(num1, num2, op.getSymbol());
 
-// ArrayList 활용
-//        ArrayList<Double> list = new ArrayList<>();
-//
-//        System.out.println("=========== 계산기 시작 ===========");
-//        Scanner sc = new Scanner(System.in);
-//        Calculator calc = new Calculator();
-//        while (true) {
-//
-//            boolean flag = false;
-//
-//            System.out.println("정수를 입력하세요(exit 입력 시 종료) : ");
-//            String input1 = sc.nextLine();
-//
-//            //종료할지 안할지 확인
-//            if (input1.equals("exit")) {//입력 받은 값이 exit인지 확인
-//                System.out.println("=========== 계산기 종료 ===========");
-//                break;
-//
-//            } else if (input1.matches("-?\\d+")) { //숫자인지 확인
-//                System.out.println("정수를 입력하세요:");
-//                String input2 = sc.nextLine();
-//
-//                if (input2.matches("-?\\d+")) { //숫자인지 확인
-//                    int num1 = Integer.parseInt(input1);
-//                    int num2 = Integer.parseInt(input2);
-//                    double result = 0 ;
-//                    boolean flag2 = true;
-//
-//                    System.out.println("사칙연산을 입력하세요 (+, -, *, /) : ");
-//                    String c = sc.nextLine();
-//
-//                    try{
-//                        switch (c) {
-//                            case "+" :
-//                                result=calc.sum(num1,num2);
-//
-//                                break;
-//                            case "-" :
-//                                result=calc.sub(num1,num2);
-//
-//                                break;
-//                            case "*" :
-//                                result = calc.mul(num1,num2);
-//
-//                                break;
-//                            case "/" :
-//                                result = calc.div(num1,num2);
-//                                break;
-//                            default :
-//                                System.out.println("사칙연산이 아닙니다.");
-//                                flag2 = false;
-//                                break;
-//                        }
-//                    } catch (ArithmeticException e) {
-//                        System.out.println(e.getMessage());
-//                        flag2 = false;
-//                    }
-//                    if (flag2){//ArrayList 데이터 추가
-//                        System.out.println("결과 : "+result);
-//                        list.add(result);
-//                        flag = true;
-//                    }
-//
-//                } else {
-//                    System.out.println("정수가 아닙니다.");
-//                }
-//            } else {
-//                System.out.println("정수나 exit만 입력 가능합니다.\n다시 입력하세요");
-//            }
-//
-//            while (flag) {
-//
-//                System.out.println("기록을 확인 하시겠습니까? (Y/N)");
-//                String input3 = sc.nextLine();
-//                if (input3.equals("Y") || input3.equals("y")) {
-//
-//                    System.out.println("=====지금까지 계산한 합계 조회=====");
-//                    System.out.println(list);
-//
-//                    break;
-//                }
-//                else if (input3.equals("N")|| input3.equals("n")) {
-//                    break;
-//                }
-//                else {
-//                    System.out.println("형식에 맞게 입력해주세요");
-//                }
-//            }
-//            while (flag) { //ArrayList 데이터 삭제
-//                System.out.println("처음으로 기록된 데이터를 삭제 하시겠습니까? (Y/N)");
-//                String input4 = sc.nextLine();
-//                if (input4.equals("Y") || input4.equals("y")) {
-//                    list.remove(0);// 처음 기록된 데이터를 삭제한다.
-//                    System.out.println("=====지금까지 계산한 합계 조회=====");
-//                    System.out.println(list);
-//                    break;
-//
-//                }
-//                else if (input4.equals("N")|| input4.equals("n")) {
-//                    break;
-//                }
-//                else {
-//                    System.out.println("형식에 맞게 입력해주세요");
-//                }
-//            }
-//        }
+
+                    } catch (ArithmeticException | IllegalArgumentException e) {
+                        System.out.println(e.getMessage());
+
+                    }
+                    break;
+
+                case "2":
+                    while (true) {
+                        System.out.println("기록을 확인 하시겠습니까? (Y/N)");
+                        String recordinput = sc.nextLine();
+                        if (recordinput.equals("Y") || recordinput.equals("y")) {
+                            System.out.println("기록");
+                        } else if (recordinput.equals("N") || recordinput.equals("n")) {
+                            break;
+                        } else {
+                            System.out.println("형식에 맞게 입력해주세요");
+                        }
+                    }
+                    break;
+                case "3":
+                    while (true) { //ArrayList 데이터 삭제
+
+                        System.out.println("처음으로 기록된 데이터를 삭제 하시겠습니까? (Y/N)");
+                        String deleteinput = sc.nextLine();
+                        try {
+
+                            if (deleteinput.equals("Y") || deleteinput.equals("y")) {
+
+                                break;
+
+                            } else if (deleteinput.equals("N") || deleteinput.equals("n")) {
+                                break;
+                            } else {
+                                System.out.println("형식에 맞게 입력해주세요");
+                            }
+                        } catch (IndexOutOfBoundsException e) {
+                            System.out.println("삭제할 값이 없습니다.");
+                            break;
+                        }
+                    }
+                    break;
+
+                case "exit"://입력 받은 값이 exit인지 확인
+                    System.out.println("=========== 계산기 종료 ===========");
+                    return;
+                default:
+                    System.out.println("형식에 맞게 입력해주세요.");
+                    break;
+
+            }
+        }
     }
 }
--- src/lv3/ArithmeticCalculator.java
+++ src/lv3/ArithmeticCalculator.java
@@ -1,5 +1,46 @@
 package lv3;
 
-public class ArithmeticCalculator {
+import java.util.ArrayList;
+
+public class ArithmeticCalculator<T extends Number> {
+    private double result;
+    private boolean flag;
+
+
+    private ArrayList<Double> list = new ArrayList<>();
+
+    public ArithmeticCalculator() {
+
+    }
+
+    public void calculator(T num1, T num2, String ch) {
+        switch (ch) {
+            case "+":
+                result = num1.doubleValue() + num2.doubleValue();
+                System.out.println(result);
+                list.add(result);
+
+                break;
+            case "-":
+                result = num1.doubleValue() - num2.doubleValue();
+                System.out.println(result);
+                list.add(result);
+                break;
+            case "*":
+                result = num1.doubleValue() * num2.doubleValue();
+                System.out.println(result);
+                list.add(result);
+                break;
+            case "/":
+                if (num2.doubleValue() == 0) {
+                    throw new ArithmeticException("0으로 나눌 수 없습니다.");//예외 의도 적으로 발생 시킴
+                }
+                result = num1.doubleValue() / num2.doubleValue();
+                System.out.println(result);
+                list.add(result);
+                break;
+        }
+    }
+
 
 }
